f50d77f26343977ffc193de7926ed1abb053d8b2
Nikita Koynov <nikicha03@gmail.com>
2020-03-29 17:03:38 +0300

    added basic broadcast functionality

--- common/msgBus.js
+++ common/msgBus.js
@@ -5,24 +5,33 @@
 
     Трябва да има два основни топика - system и service.
     */
-var msgBus = {
+   var MsgBus = {
     //Връща комуникатор с помощта, на който услугата може да изпраща различен тип съобщения.
     init: () => {
-        
+        this.broadcastSubs = [];
+        this.querySubs = {};
+        return MsgBus;
     },
     //{"source": "discovery", "system": "ping"}
     //Приема метод който се извиква при получаване на broadcast, не връща резултат.
-    broadcast: () => {
-        
+    subscribeBroadcast: (callBack) => {
+        this.broadcastSubs.push(callBack);
     },
     //Приема метод който се извиква след получаване на query, не връща резултат.
-    subscribeQuery: (query, action) => {
-        
+    subscribeQuery: (query, callBack) => {
+        // if (!(query in this.querySubs)) {
+        //     //creates new arr for callbacks for this query
+        //     this.querySubs[query] = [];
+        // }
+
+        // this.querySubs[query].push(callBack);
     },
     //{"source": "discovery", "system": "pong", "host": "hostname", "name": "microservice name", uptime: 12.22}
     //Параметри: payload - метод който изпраща съобщения и не чака отговор
     broadcast: (payload) => {
-        
+        this.broadcastSubs.forEach(sub => {
+            sub(payload);
+        });
     },
     //Параметри: subject - получател, payload. Връща Promise, който resolve-a  с отговора или reject-ва с грешка.
     query(subject, payload) {
