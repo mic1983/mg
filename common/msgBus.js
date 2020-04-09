e60067602c48a23bc2215bb0c5bc0f2d9f63c19e
Nikita Koynov <nikicha03@gmail.com>
2020-04-10 02:22:19 +0300

    updated msgBus query

--- common/msgBus.js
+++ common/msgBus.js
@@ -8,28 +8,37 @@ class MsgBus {
 
     //Creates new message bus instance
     constructor() {
+        const os = require("os"); 
+        this._hostname = os.hostname();
+
         //Broadcast subscribers
         this._broadcastSubs = [];
+
         //Query subscribers
         this._querySubs = {};
+        
         //Ids of sended but not answerd queries
-        this._pendingQueryIds = [];
+        this._pendingQuerys = new Map();
     }
 
     //Creates connection for Redis
-    async init() {
-        let connection = require("../redis-connection.json")
+    init() {
+        return new Promise(async (resolve, reject) => {
 
-        let config = {
-            port: connection.port,
-            host: connection.host,
-            auth: connection.auth,
-            scope: connection.scopes.service
-        };
+            let connection = require("../redis-connection.json")
 
-        this.nrp = new NRP(config);
+            let config = {
+                port: connection.port,
+                host: connection.host,
+                auth: connection.auth,
+                scope: connection.scopes.service
+            };
 
-        await this.nrp.on("broadcast", (payload) => this.notifySubscribers(payload));
+            this.nrp = new NRP(config);
+
+            await this.nrp.on(this._hostname, (payload) => this.finishQuery(payload))
+            this.nrp.on("broadcast", (payload) => this.notifyLocalSubscribers(payload), () => resolve());
+        })
     }
 
     //Parameters: callBack - function to be subscribed
@@ -43,8 +52,11 @@ class MsgBus {
     //Subscribes a function which will be invoked on query
     //Returns Promise<void>
     async subscribeQuery(subject, callBack) {
-        //Change callBack with msgBus function
-        await this.nrp.on(subject, callBack);
+        if (!this._querySubs.hasOwnProperty(subject)) {
+            this._querySubs[subject] = [];
+        }
+
+        this._querySubs[subject].push(callBack);
     }
 
     //Parameters: payload - object to send
@@ -54,54 +66,82 @@ class MsgBus {
         await this.nrp.emit("broadcast", payload);
     }
 
-    async notifySubscribers(payload) {
-        this._broadcastSubs.forEach(sub => {
-            sub(payload);
-        });
-    }
-
     //Parameters: subject - the purpose of the query, payload - object to send
     //Sends the payload to all subscribers to the specific query
     //Returns Promise<Object> - received answer
-    async query(subject, payload) {
-        let queryPackage = await this.createQueryPackage(subject, payload);
-        console.log(queryPackage);
+    query(subject, payload) {
+        let promise =  new Promise((resolve, reject) => {
+            this.createQueryPackage(subject, payload, resolve)
+                .then((queryPackage) => this.nrp.emit("broadcast", queryPackage))
+        });
 
-        await this.nrp.emit(subject, payload);
+        return promise;
     }
 
-    async createQueryPackage(subject, payload) {
-        let hostname = await MsgBus.getHostname();
+    async createQueryPackage(subject, payload, resolve) {
         let id = await MsgBus.generateGuid();
 
-        this._pendingQueryIds.push(id);
+        this._pendingQuerys.set(id, resolve);
 
         let queryPackage = {
             "query": subject,
             "payload": payload,
             "query-id": id,
-            "node": hostname
+            "node": this._hostname
         };
 
         return queryPackage;
     }
 
-    async finishQuery(id) {
-        let index = this._pendingQueryIds.indexOf(id);
+    async finishQuery(resultPackage) {
+        let result = resultPackage.result;
+        let id = resultPackage["query-id"];
 
-        if (index > -1) {
-            array.splice(index, 1);
-        }
-        else {
-            throw Error("Can't find query!");
+        let resolve = this._pendingQuerys.get(id);
+
+        resolve(result);
+
+        this._pendingQuerys.delete(id);
+    }
+
+    async notifyLocalSubscribers(payload) {
+        let isQuery = payload.hasOwnProperty("query-id");
+
+        if (isQuery) {
+            await this.notifyLocalQuerySubscribers(payload);
+        } else {
+            await this.notifyLocalBroadcastSubscribers(payload);
         }
     }
-    
-    static async getHostname() {
-        const os = require("os");
-        let hostname = os.hostname();
 
-        return hostname;
+    async notifyLocalBroadcastSubscribers(payload) {
+        this._broadcastSubs.forEach(sub => {
+            sub(payload);
+        });
+    }
+
+    async notifyLocalQuerySubscribers(queryPackage) {
+        let query = queryPackage.query;
+        let node = queryPackage.node;
+        let payload = queryPackage.payload;
+
+        //TODO check whether there is valid querySub
+        if (!this._querySubs.hasOwnProperty(query)) {
+            //throw Error("Not registered query subject!");
+        }
+
+        let callBack = this._querySubs[query][0];
+        let result = callBack(payload);
+
+        let resultPackage = {
+            "query": query,
+            "payload": payload,
+            "query-id": queryPackage["query-id"],
+            "node": node,
+            "result": result
+        };
+
+        await this.nrp.emit(node, resultPackage);
     }
 
     static async generateGuid() {
