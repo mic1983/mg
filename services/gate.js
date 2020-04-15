0ae8a1796fe6cad88ebdc6ed07459cadd2a12016
kripri <krisio123123@gmail.com>
2020-04-15 12:31:07 +0300

    gate service

--- services/gate.js
+++ services/gate.js
@@ -1,11 +1,30 @@
 // TODO gate service
-const io = require('socket.io')(3000)
-io.on('connection', socket =>{
 
-})
-const socket = io('http://localhost:3000')
- 
-class Gate{
-async init()
+
+class Gate {
+    async init() {
+       
+        this.app.get('/', (req, res) => {
+            res.sendFile(__dirname + '/index.html');
+        });
+        
+        this.io.on('connection', socket => {
+            console.log('function connected');
+        });
+        
+        this.http.listen(3000, () => {
+            console.log('listening on *:3000');
+        });
+        this.io.on('disconnect', () => {
+            console.log('function disconnected');
+        })
+    }
+    constructor(){
+        this.app = require('express')();
+        this.http = require('http').createServer(this.app);
+        this.io = require('socket.io')(this.http);
+        
+    }
 }
-module.exports = Gate;
+let gate = new Gate();
+gate.init();
