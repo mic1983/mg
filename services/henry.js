3e280737706cba4890ed4bd89225bde5eb953d2f
Mihail Denev <denev@bismartsolutions.com>
2020-05-22 15:57:26 +0300

    added example services

--- services/henry.js
+++ services/henry.js
@@ -1,1 +1,18 @@
-// TODO simple service
+class HenryService {
+    async init(msgBus, log) {
+        this.msgBus = msgBus;
+        this.logService = log;
+
+        this.logService.log('Henry initialised');
+        this.msgBus.subscribeQuery('henry-test', async (payload) => {
+            this.logService.log('Henry answer to test query');
+            return {
+                message: 'Henry answer to test query'
+            };
+        });
+
+        return this;
+    }
+}
+
+module.exports = HenryService;
--- services/ross.js
+++ services/ross.js
@@ -1,1 +1,18 @@
-// TODO simple service
+class RossService {
+    async init(msgBus, log) {
+        this.msgBus = msgBus;
+        this.logService = log;
+
+        this.logService.log('Ross initialised');
+        this.msgBus.subscribeQuery('ross-test', async (payload) => {
+            this.logService.log('Ross answer to test query');
+            return {
+                message: 'Ross answer to test query'
+            }
+        });
+
+        return this;
+    }
+}
+
+module.exports = RossService;
