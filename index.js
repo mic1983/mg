99e8f20dc4de021bf1f0a11d13b9c32bc85248b0
Mihail Denev <denev@bismartsolutions.com>
2020-05-22 15:58:09 +0300

    implement index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const LogService = require("./common/log");
+const MessageBus = require("./common/msgBus");
+const GateService = require("./services/GateService/gate");
+const HenryService = require("./services/henry");
+const RossService = require("./services/ross");
 
+const connectionSettings = require("./redis-settings.json")
+
+async function main() {
+    const log = await (new LogService()).init();
+    const msgBus = await (new MessageBus()).init(connectionSettings, log);
+
+    //services
+    const gate = await (new GateService()).init(msgBus, log);
+    const henry = await(new HenryService()).init(msgBus, log);
+    const ross = await(new RossService()).init(msgBus, log);
+}
+
+
+main();
