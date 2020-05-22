90bd43848572214702f35b1c75954e36b893960a
Mihail Denev <denev@bismartsolutions.com>
2020-05-22 13:41:55 +0300

    refactor log service

--- common/log.js
+++ common/log.js
@@ -1,13 +1,29 @@
-//TODO log service
-class LogService{
+class LogService {
   async log(message, arg) {
     const process = require('process');
+
+    var uptime = process.uptime();
+    const date = new Date(uptime*1000);
+
+    const minutes = date.getUTCMinutes();
+    const seconds = date.getUTCSeconds();
+    const milliseconds = date.getUTCMilliseconds();
+
+    const time = minutes +':'+ seconds +'.'+ milliseconds;
+
     if(typeof arg !== 'undefined' && arg !== null){
-      console.log(`[${process.uptime()*1000}] ${message} {${arg}}`);
+      console.log(`[${time}] ${message} {${arg}}`);
     }
     else {
-    console.log(`[${process.uptime()*1000}] ${message}`);
+    console.log(`[${time}] ${message}`);
     }
   }
+
+  //same interface for initialisation for all services
+  async init() {
+    this.log("LogService initialised");
+    return this;
+  }
 }
-module.exports.LogService = LogService;
+
+module.exports = LogService;
